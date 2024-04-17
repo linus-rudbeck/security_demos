5232e697992379a1388a26582747a8ebcaf5c54d
linus-rudbeck <linus.rudbeck@distansakademin.se>
2024-04-17 15:54:08 +0200

    Update AsymmetricEncryption.java

--- src/main/java/se/distansakademin/AsymmetricEncryption.java
+++ src/main/java/se/distansakademin/AsymmetricEncryption.java
@@ -1,18 +1,27 @@
 package se.distansakademin;
 
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.*;
+import java.util.Base64;
 
 public class AsymmetricEncryption {
 
-    public static void main(String[] args) throws NoSuchAlgorithmException {
+    public static void main(String[] args) throws Exception {
+
+        var secret = "Attackera ingen, vi är fredliga varelser!";
 
         var pair = getKeyPair();
-        System.out.println(pair.getPrivate().toString());
-        System.out.println("---");
-        System.out.println(pair.getPublic().toString());
 
+        var encrypted = encryptText(secret, pair.getPublic());
+
+        System.out.println(encrypted);
+
+        var result = decryptText(encrypted, pair.getPrivate());
+
+        System.out.println(result);
     }
 
     private static KeyPair getKeyPair() throws NoSuchAlgorithmException {
@@ -20,5 +29,24 @@ public class AsymmetricEncryption {
         keyGen.initialize(2048);
         var keyPair = keyGen.generateKeyPair();
         return keyPair;
+    }
+
+    private static String encryptText(String secret, PublicKey publicKey) throws Exception{
+        var cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        var encryptedBytes = cipher.doFinal(secret.getBytes());
+        var encryptedText = Base64.getEncoder().encodeToString(encryptedBytes);
+
+        return encryptedText;
+    }
+
+    private static String decryptText(String encryptedText, PrivateKey privateKey) throws Exception{
+        var cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        var encryptedBytes = Base64.getDecoder().decode(encryptedText);
+        var decryptedBytes = cipher.doFinal(encryptedBytes);
+        var decryptedText = new String(decryptedBytes);
+
+        return decryptedText;
     }
 }
